ba630c5cca58d1cc14b3a703bc7647a022b147aa
Alexandra Deinekina <alexandra.calatorim@gmail.com>
2021-09-23 01:31:17 +0300

    second commit

---
+++ InputValidationHW/InputValidationHW/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputValidationHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+////            Email
+////  regex: (?:[a - z0 - 9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
+////  url: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+
+////Phone
+////  regex: ^\+[1 - 9]\d{ 1,14}$
+////  url: https://ihateregex.io/expr/e164-phone/
+
+////            Dates
+////              regex: ^(0[1 - 9] | 1[012])[- /.](0[1 - 9] |[12][0 - 9] | 3[01])[- /.](19 | 20)\d\d$
+////  url: https://www.regular-expressions.info/dates.html
+
+////            Zip Code(4 digits)
+////  regex: ^[0 - 9]{ 4}$
+////  url: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s14.html#:~:text=You%20need%20to%20validate%20a,123456789%20%2C%20or%201234%2D56789%20.
+
+////            URLs
+////              regex: (https ?:\/\/ (?: www\.| (? !www))[a-zA - Z0 - 9][a-zA - Z0 - 9 -]+[a - zA - Z0 - 9]\.[^\s]{ 2,}| www\.[a-zA - Z0 - 9][a-zA - Z0 - 9 -]+[a - zA - Z0 - 9]\.[^\s]{ 2,}| https ?:\/\/ (?: www\.| (? !www))[a-zA - Z0 - 9]+\.[^\s]{ 2,}| www\.[a-zA - Z0 - 9]+\.[^\s]{ 2,})
+////  url: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url\
+
+
+////            Сайт для тестов: http://regexstorm.net/tester
+        }
+    }
+}
